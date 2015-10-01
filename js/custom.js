4a24989cf6083ce498837f0a1a515d0709f567ec
Gianluca Arbezzano <gianarb92@gmail.com>
2015-10-01 11:11:03 +0200

    refactoring events

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,14 @@
 jQuery(document).ready(function () {
 
-    $('#gf').text('GitHub Followers');
-    $('#gfr').text('GitHub Repos');
-
-    JSONP('https://api.github.com/users/ocramius?callback=?', function (response) {
-        var data = response.data;
-        $('#gf').text(data.followers + ' GitHub Followers');
-        $('#gfr').text(data.public_repos + ' GitHub Repos');
-    });
-
-    function JSONP(url, callback) {
-        var id = ( 'jsonp' + Math.random() * new Date() ).replace('.', '');
-        var script = document.createElement('script');
-        script.src = url.replace('callback=?', 'callback=' + id);
-        document.body.appendChild(script);
-        window[ id ] = function (data) {
-            if (callback) {
-                callback(data);
-            }
-        };
+  var navigatorSwitchActive = function() {
+    var els = document.querySelectorAll("#top a");
+    $(".nav").find("#top .active").removeClass("active");
+    for (var ii = 0; ii < els.length; ii++) {
+      if (els[ii].href === location.href) {
+        $(els[ii]).parent().addClass("active");
+        return;
+      }
     }
-
-    $('#ghw').githubWidget({
-        'username':                   'Ocramius',
-        'displayActions':             false,
-        'firstCount':                 10,
-        'displayHeader':              false,
-        'displayLastCommit':          false,
-        'displayAccountInformations': false,
-        'displayLanguage':            false
-    });
+  };
+  navigatorSwitchActive();
 });
