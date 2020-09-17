5b4f73f98b41c6fe59687559d16a71eb38810e91
Gianluca Arbezzano <gianarb92@gmail.com>
2020-09-17 18:44:54 +0200

    nix-os

---
+++ default.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> { };
+let jekyll_env = bundlerEnv rec {
+    name = "jekyll_env";
+    gemfile = ./Gemfile;
+    lockfile = ./Gemfile.lock;
+    gemset = ./gemset.nix;
+  };
+in
+  stdenv.mkDerivation rec {
+    name = "jekyll_env";
+    buildInputs = [ jekyll_env ];
+
+    shellHook = ''
+      exec ${jekyll_env}/bin/jekyll serve -w --future --drafts
+    '';
+  }
