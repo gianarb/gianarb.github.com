f0dabbeffc091a8cbd50a6b2fef847be3cc7e059
Gianluca Arbezzano <gianarb92@gmail.com>
2021-02-10 09:01:47 +0100

    remove js

--- default.nix
+++ default.nix
@@ -1,16 +1,16 @@
 with import <nixpkgs> { };
 let jekyll_env = bundlerEnv rec {
-    name = "jekyll_env";
-    gemfile = ./Gemfile;
-    lockfile = ./Gemfile.lock;
-    gemset = ./gemset.nix;
-  };
+  name = "jekyll_env";
+  gemfile = ./Gemfile;
+  lockfile = ./Gemfile.lock;
+  gemset = ./gemset.nix;
+};
 in
-  stdenv.mkDerivation rec {
-    name = "jekyll_env";
-    buildInputs = [ jekyll_env ];
+stdenv.mkDerivation rec {
+  name = "jekyll_env";
+  buildInputs = [ jekyll_env nodejs ];
 
-    shellHook = ''
-      exec ${jekyll_env}/bin/jekyll serve -w --future --drafts
-    '';
-  }
+  shellHook = ''
+    alias serve="${jekyll_env}/bin/jekyll serve -w --future --drafts"
+  '';
+}
