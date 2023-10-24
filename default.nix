c5d52903169d6bee69218029b28c936bc136844e
Gianluca Arbezzano <ciao@gianarb.it>
2023-10-24 11:31:03 +0200

    chore: nixos, action and gem update

--- default.nix
+++ default.nix
@@ -1,12 +1,14 @@
-with import <nixpkgs> { };
-let jekyll_env = bundlerEnv rec {
-  name = "jekyll_env";
-  gemfile = ./Gemfile;
-  lockfile = ./Gemfile.lock;
-  gemset = ./gemset.nix;
-};
-in
-stdenv.mkDerivation rec {
-  name = "jekyll_env";
-  buildInputs = [ jekyll_env nodejs ];
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    glibc
+    jekyll
+    ruby
+
+    libyaml
+    libffi
+    zlib
+    pkg-config
+    readline
+  ];
 }
